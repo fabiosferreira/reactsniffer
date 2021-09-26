8db8e63254251dd9eba59230829dc24f450685cf
Fábio Ferreira <fabio.ferreira@ifsudestemg.edu.br>
2021-09-26 14:43:37 -0300

    Chanding read file method

--- src/utils/read_files.js
+++ src/utils/read_files.js
@@ -7,19 +7,25 @@ module.exports = function(dirname){
 
 var walk = function(dir) {
     var files = [];
-    var list = fs.readdirSync(dir);
-    list.forEach(function(file) {
-        file = path.join(dir, file);
-        var stat = fs.statSync(file);
-        // Is a directory
-        if (stat && stat.isDirectory()) { 
-            // Recurse into a subdirectory
-            files = files.concat(walk(file));
-        } else { 
-            // Filtering js files          
-            if (file.endsWith(".js") || file.endsWith(".jsx") || file.endsWith(".ts") || file.endsWith(".tsx"))
-                files.push(file);
-        }
-    });
+
+    if (fs.existsSync(dir)){
+        var allFiles = fs.readdirSync(dir);
+        allFiles.forEach(function(file) {
+            file = path.join(dir, file);
+            var stat = fs.statSync(file);
+            // Is a directory
+            if (stat && stat.isDirectory()) { 
+                // Recurse into a subdirectory
+                files = files.concat(walk(file));
+            } else { 
+                // Filtering js files          
+                if (file.endsWith(".js") || file.endsWith(".jsx") || file.endsWith(".ts") || file.endsWith(".tsx"))
+                    files.push(file);
+            }
+        });
+    }else{
+        console.log("\x1B[31mNo such file or directory: ", dir);
+        process.exit(1);
+    }
     return files;
 }
